fbf7939d16df3009184a8381d4b0abf34dc833f4
GermanBarrera <lbarrera@solvd.com>
2023-11-06 18:48:11 -0300

    Selenium support was added to the pom. It was created ChangeLanguageEnglishTest with its class, but some refactor is needed.

---
+++ src/main/java/pages/YourAccountPage.java
@@ -0,0 +1,37 @@
+package pages;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.example.App;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class YourAccountPage {
+    private static final Logger LOGGER = LogManager.getLogger(App.class);
+    private WebDriver driver;
+
+    public YourAccountPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    private By pageTitle = By.cssSelector("[class=\"a-row a-spacing-base\"]");
+
+    public boolean isTitlePresent() {
+        LOGGER.info("Here it is getting the title element");
+        boolean result = false;
+        try {
+            WebDriverWait wait = new WebDriverWait(driver,Duration.ofMillis(5000));
+            return result = wait.until(ExpectedConditions.visibilityOf(
+                    driver.findElement(pageTitle))).isDisplayed();
+        }catch (Exception e){
+            LOGGER.error("Error getting page title");
+        }
+        return false;
+    }
+
+
+}
