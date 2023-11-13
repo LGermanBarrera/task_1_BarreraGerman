0348cf0bf456f8d690b065d7507a0f1fdd7e56cb
GermanBarrera <lbarrera@solvd.com>
2023-11-13 12:39:00 -0300

    It was refactored Factory Page model from selenium.

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
@@ -15,48 +17,72 @@ import java.util.List;
 public class HomePage {
     private static final Logger LOGGER = LoggerFactory.getLogger(HomePage.class);
     private WebDriver driver;
-    private By logo = By.cssSelector("[aria-label=\"Amazon\"]");
-    private By signIn = By.cssSelector("#nav-link-accountList-nav-line-1");
+    //    private By logo = By.cssSelector("[aria-label=\"Amazon\"]");
+    @FindBy(css = "[aria-label=\"Amazon\"]")
+    private WebElement logo;
+    //    private By signIn = By.cssSelector("#nav-link-accountList-nav-line-1");
+    @FindBy(css = "#nav-link-accountList-nav-line-1")
+    private WebElement signIn;
 
-    private By burgerButton = By.cssSelector("i[class=\"hm-icon nav-sprite\"]");
+    @FindBy(css = "i[class=\"hm-icon nav-sprite\"]")
+    private WebElement burgerButton;
+//    private By burgerButton = By.cssSelector("i[class=\"hm-icon nav-sprite\"]");
 //    private By burgerButton = By.cssSelector("a#nav-logo-sprites");
 
-    private By greetingMessage = By.cssSelector("#hmenu-customer-profile-right");
-    private By changeLang = By.cssSelector("[class='icp-nav-link-inner']");
+    @FindBy(css = "#hmenu-customer-profile-right")
+    private WebElement greetingMessage;
+    //    private By greetingMessage = By.cssSelector("#hmenu-customer-profile-right");
+    @FindBy(css = "[class='icp-nav-link-inner']")
+    private WebElement changeLang;
+//    private By changeLang = By.cssSelector("[class='icp-nav-link-inner']");
 
-    private By selectLanguages = By.cssSelector(".nav-a.nav-a-2.icp-link-style-2");
+    @FindBy(css = ".nav-a.nav-a-2.icp-link-style-2")
+    private WebElement selectLanguages;
+    //    private By selectLanguages = By.cssSelector(".nav-a.nav-a-2.icp-link-style-2");
     //            = By.cssSelector("div#nav-flyout-icp>div.nav-tpl-itemList >a");
-    private By nineLanguages = By.cssSelector("div#nav-flyout-icp>div.nav-template.nav-flyout-content.nav-tpl-itemList>a[href^='#switch'] ");
+    @FindBy(css = "div#nav-flyout-icp>div.nav-template.nav-flyout-content.nav-tpl-itemList>a[href^='#switch']")
+    private WebElement nineLanguages;
+//    private By nineLanguages = By.cssSelector("div#nav-flyout-icp>div.nav-template.nav-flyout-content.nav-tpl-itemList>a[href^='#switch'] ");
 
-    private By englishLang = By.cssSelector("div#nav-flyout-icp-footer-flyout>div.nav-template.nav-flyout-content.nav-tpl-itemList, a[href=\"#switch-lang=en_US\"]");
+    @FindBy(css = "div#nav-flyout-icp-footer-flyout>div.nav-template.nav-flyout-content.nav-tpl-itemList, a[href=\"#switch-lang=en_US\"]")
+    private WebElement englishLang;
+    //    private By englishLang = By.cssSelector("div#nav-flyout-icp-footer-flyout>div.nav-template.nav-flyout-content.nav-tpl-itemList, a[href=\"#switch-lang=en_US\"]");
     //    private By englishLang = By.cssSelector("div#nav-flyout-icp-footer-flyout>div.nav-template.nav-flyout-content.nav-tpl-itemList,a[href='#switch-lang=en_US']::first-line");
-    private By basicLogo = By.cssSelector("div.nav-bb-left a#nav-bb-logo");
+    @FindBy(css = "div.nav-bb-left a#nav-bb-logo")
+    private WebElement basicLogo;
+//    private By basicLogo = By.cssSelector("div.nav-bb-left a#nav-bb-logo");
 
-    private By englishLogo = By.cssSelector("img[alt=\"Shop Early Black Friday deals\"]");
+    @FindBy(css = "img[alt=\"Shop Early Black Friday deals\"]")
+    private WebElement englishLogo;
+//    private By englishLogo = By.cssSelector("img[alt=\"Shop Early Black Friday deals\"]");
 
-    private By menuList = By.cssSelector("div#hmenu-content>ul[class=\"hmenu hmenu-visible\"]>li");
+    @FindBy(css = "div#hmenu-content>ul[class=\"hmenu hmenu-visible\"]>li")
+    private WebElement menuList;
+//    private By menuList = By.cssSelector("div#hmenu-content>ul[class=\"hmenu hmenu-visible\"]>li");
 
-    private By yourAccount = By.cssSelector("div#hmenu-content [class=\"hmenu hmenu-visible\"]>li a[href$=\"1_35\"]");
+    @FindBy(css = "div#hmenu-content [class=\"hmenu hmenu-visible\"]>li a[href$=\"1_35\"]")
+    private WebElement yourAccount;
+//    private By yourAccount = By.cssSelector("div#hmenu-content [class=\"hmenu hmenu-visible\"]>li a[href$=\"1_35\"]");
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
     public boolean isHomePageOpened() {
-        return driver.findElement(logo).isDisplayed();
+        return logo.isDisplayed();
     }
 
     public SignInPage clickOnSignIn() {
-        driver.findElement(signIn).click();
+        signIn.click();
         return new SignInPage(driver);
     }
 
     public void clickOnBurgerButton() {
         try {
             WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-            wait.until(ExpectedConditions.visibilityOf(
-                    driver.findElement(burgerButton))).isDisplayed();
-            driver.findElement(burgerButton).click();
+            wait.until(ExpectedConditions.visibilityOf(burgerButton));
+            burgerButton.click();
             LOGGER.info("Burger button is clicked");
         } catch (Exception e) {
             LOGGER.error("Burger button was not clicked");
@@ -67,42 +93,41 @@ public class HomePage {
         try {
             WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
             wait.until(ExpectedConditions.visibilityOf(
-                    driver.findElement(greetingMessage))).isDisplayed();
+                    greetingMessage));
+            return greetingMessage.isDisplayed();
 //            return driver.findElement(greetingMessage).isDisplayed();
         } catch (Exception e) {
-           LOGGER.info("Burger button was not clicked");
+            LOGGER.info("Burger button was not clicked");
         }
-        return driver.findElement(greetingMessage).isDisplayed();
+        return greetingMessage.isDisplayed();
     }
 
     public LanguagePage clickOnLanguages() {
-        driver.findElement(changeLang).click();
+        changeLang.click();
         return new LanguagePage(driver);
     }
 
     public HomePage hoverOverChangeLanguage() {
-        WebElement langList = driver.findElement(selectLanguages);
+        WebElement langList = driver.findElement(By.cssSelector(selectLanguages.getText()));
         Actions action = new Actions(driver);
         action.moveToElement(langList).perform();
         try {
             WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(20000));
-            wait.until(ExpectedConditions.visibilityOf(
-                    driver.findElement(englishLang))).isDisplayed();
+            wait.until(ExpectedConditions.visibilityOf(englishLang));
         } catch (Exception e) {
-
             LOGGER.error("English language was not selected " + e);
         }
-
         return this;
     }
 
     public HomePage clickIfOldPageIsPresent() {
+        boolean result = false;
         try {
-            if (driver.findElement(basicLogo).isDisplayed()) ;
-            driver.findElement(basicLogo).click();
-            return new HomePage(driver);
+            if (basicLogo.isDisplayed())
+//            driver.findElement(basicLogo).click();
+                return new HomePage(driver);
         } catch (Exception e) {
-          LOGGER.info("The old page was not clicked" +e);
+            LOGGER.info("The old page was not clicked" + e);
             throw new RuntimeException(e);
         } finally {
             return new HomePage(driver);
@@ -110,17 +135,17 @@ public class HomePage {
     }
 
     public List<WebElement> printLanguageList() {
-        List<WebElement> languageList = driver.findElements(nineLanguages);
+        List<WebElement> languageList = driver.findElements(By.cssSelector(nineLanguages.getText()));
         return languageList;
     }
 
     public List<WebElement> burgerButtonList() {
-        List<WebElement> list = driver.findElements(menuList);
+        List<WebElement> list = driver.findElements(By.cssSelector(menuList.getText()));
         return list;
     }
 
     public YourAccountPage clickOnYourAccount() {
-        driver.findElement(yourAccount).click();
+        yourAccount.click();
         return new YourAccountPage(driver);
     }
 }
